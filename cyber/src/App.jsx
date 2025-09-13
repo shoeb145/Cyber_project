83c89c0c66f6b966dee2a24bf69642e22f3aaed3
Rahul-Aitla <aitlarahul@gmail.com>
2025-09-13 19:57:06 +0530

    app.jsx added

--- cyber/src/App.jsx
+++ cyber/src/App.jsx
@@ -4,12 +4,14 @@ import './home_page.css';
 import Hero from './components/Hero';
 import Home_page from './components/Home_page';
 import BenefitsSection from './components/BenefitsSection';
+import VideoSection from './components/VideoSection';
 function App() {
   return (
     <div className="App">
       <Hero />
       <Home_page />
       <BenefitsSection />
+      <VideoSection />
     </div>
   );
 }
